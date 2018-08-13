6726a61109a40c86722e13b4e724076ac6c22f59
ShauralP <patel675@purdue.edu>
2018-08-13 14:59:31 -0400

    Setup Android project for Simple MNIST

---
+++ SimpleMNIST/Android/app/src/main/java/com/example/shaur/simple_mnist_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.shaur.simple_mnist_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
