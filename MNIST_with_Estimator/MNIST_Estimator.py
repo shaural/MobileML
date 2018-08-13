5f4b5ff339194e93c12716ac3bf05d73c759cd6f
ShauralP <patel675@purdue.edu>
2018-08-13 17:42:50 -0400

    Added prediction code

--- MNIST_with_Estimator/MNIST_Estimator.py
+++ MNIST_with_Estimator/MNIST_Estimator.py
@@ -6,6 +6,7 @@ x_train = mnist_data.train.images
 y_train  = np.asarray(mnist_data.train.labels, dtype=np.int32)
 x_eval = mnist_data.test.images
 y_eval = np.asarray(mnist_data.test.labels, dtype=np.int32)
+x_predict = x_eval[:1]
 
 
 # y = Wx + b
@@ -14,6 +15,9 @@ def model_fn(features, labels, mode):
     W = tf.get_variable(name='W', shape=[784, 10], dtype=tf.float32)
     b = tf.get_variable(name='b', shape=[10], dtype=tf.float32)
     y = tf.add(tf.matmul(x, W), b)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=tf.nn.softmax(logits=y))
 
     one_hot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
     loss = tf.losses.softmax_cross_entropy(onehot_labels=one_hot_labels, logits=y)
@@ -41,6 +45,10 @@ eval_input_fn = tf.estimator.inputs.numpy_input_fn(x={'x': x_eval},
                                                    y=y_eval,
                                                    num_epochs=1,
                                                    shuffle=False)
+predict_input_fn = tf.estimator.inputs.numpy_input_fn(x={'x': x_predict},
+                                                   num_epochs=1,
+                                                   shuffle=False)
 
 estimator.train(input_fn=train_input_fn, steps=20000)
 print(estimator.evaluate(input_fn=eval_input_fn))
+print(list(estimator.predict(input_fn=predict_input_fn)))
